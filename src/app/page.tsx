d9265cf8cc5911c76e61631c8b99455d0f0f226a
i-revivehijama <irevivehijama@gmail.com>
2025-04-30 18:07:01 +0530

    seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "./components/Header";
 import Herosection from "./components/Herosection";
 import Topbar from "./components/Topbar";
@@ -5,33 +6,33 @@ import WhatWeDo from "./components/WhatWeDo";
 import Footer from "./components/Footer";
 import OurBlog from "./components/OurBlog";
 import Testimonial from "./components/Testimonial";
-// import ContactForm from "./components/ContactForm";
 import AboutUs from "./components/AboutUs";
 import WhyChooseUs from "./components/WhyChooseUs";
 import AboutThree from "./components/aboutthree";
-// import OurTeam from "./components/OurTeam";
-
 
 export default function Home() {
   return (
-    <div>
-      <Topbar />
-      <Header />
-      <Herosection />
-      <AboutUs />
+    <>
+      {/* ✅ SEO Meta Tags Here */}
+      <Head>
+        <meta
+          name="keywords"
+          content="Best Hijama Cupping Therapy Centre in Hyderabad, Best Hijama Centre in Hyderabad for Ladies, Ladies Hijama Center in Hyderabad, Best Hijama Specialist in Hyderabad, Certified Hijama Practitioner Hyderabad, Affordable Hijama Therapy in Hyderabad, Top Hijama Specialists in Hyderabad, Hijama Therapy for Women Hyderabad, Pain Relief Hijama Centers Hyderabad, Best Hijama Center for Men and Women in Hyderabad"
+        />
+      </Head>
 
-      {/* ✅ Limit to 4 treatment plans on the homepage */}
-      <WhatWeDo limit={4} />
-
-      <WhyChooseUs />
-      {/* <ContactForm /> */}
-      {/* <OurTeam /> */}
-      <AboutThree />
-      <Testimonial />
-      <OurBlog limit={3} />
-      
-      <Footer />
-    </div>
+      <div>
+        <Topbar />
+        <Header />
+        <Herosection />
+        <AboutUs />
+        <WhatWeDo limit={4} />
+        <WhyChooseUs />
+        <AboutThree />
+        <Testimonial />
+        <OurBlog limit={3} />
+        <Footer />
+      </div>
+    </>
   );
 }
-
