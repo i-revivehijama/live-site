47a6bb9251e017da6082037ea0eb63625b0e097b
i-revivehijama <irevivehijama@gmail.com>
2025-04-30 18:22:57 +0530

    tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+
 import Header from "./components/Header";
 import Herosection from "./components/Herosection";
 import Topbar from "./components/Topbar";
@@ -13,13 +13,7 @@ import AboutThree from "./components/aboutthree";
 export default function Home() {
   return (
     <>
-      {/* ✅ SEO Meta Tags Here */}
-      <Head>
-        <meta
-          name="keywords"
-          content="Best Hijama Cupping Therapy Centre in Hyderabad, Best Hijama Centre in Hyderabad for Ladies, Ladies Hijama Center in Hyderabad, Best Hijama Specialist in Hyderabad, Certified Hijama Practitioner Hyderabad, Affordable Hijama Therapy in Hyderabad, Top Hijama Specialists in Hyderabad, Hijama Therapy for Women Hyderabad, Pain Relief Hijama Centers Hyderabad, Best Hijama Center for Men and Women in Hyderabad"
-        />
-      </Head>
+     
 
       <div>
         <Topbar />
